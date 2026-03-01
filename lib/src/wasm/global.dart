fe7c3a3200c64e4e725178970fffbb85264886f9
Seven Du <5564821+medz@users.noreply.github.com>
2026-03-01 22:27:38 +0800

    feat(wasm): implement global descriptor and runtime global

--- lib/src/wasm/global.dart
+++ lib/src/wasm/global.dart
@@ -1,19 +1,29 @@
 import 'value.dart';
 
+/// Describes a WebAssembly global variable.
 class GlobalDescriptor<T extends Value<T, V>, V extends Object?> {
+  /// Creates a global descriptor with value [value] and mutability.
   const GlobalDescriptor({required this.value, this.mutable = false});
 
+  /// Value kind of this global.
   final ValueKind<T, V> value;
+
+  /// Whether this global can be updated after initialization.
   final bool mutable;
 }
 
+/// Represents a WebAssembly global variable instance.
 class Global<T extends Value<T, V>, V extends Object?> {
+  /// Creates a global from [descriptor] and initial [value].
   Global(this._descriptor, this._value);
 
   final GlobalDescriptor<T, V> _descriptor;
   V _value;
 
+  /// Current value of this global.
   V get value => _value;
+
+  /// Updates the current value when the global is mutable.
   set value(V value) {
     if (!_descriptor.mutable) {
       throw StateError('Cannot set value of immutable global');
