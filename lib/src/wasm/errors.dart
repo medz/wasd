cde415bfb1f2c5463b64fc7c583bb70e6e0ea730
Seven Du <5564821+medz@users.noreply.github.com>
2026-03-01 22:14:41 +0800

    feat(wasm): implement WasmError hierarchy

--- lib/src/wasm/errors.dart
+++ lib/src/wasm/errors.dart
@@ -1,14 +1,18 @@
-class WebAssemblyError extends Error {
-  WebAssemblyError(this.message, {this.cause});
+abstract class WasmError extends Error {
+  WasmError(this.message, {this.cause});
 
   final String message;
   final Object? cause;
 }
 
-class CompileError extends WebAssemblyError {
+class CompileError extends WasmError {
   CompileError(super.message, {super.cause});
 }
 
-class LinkError extends WebAssemblyError {
+class LinkError extends WasmError {
   LinkError(super.message, {super.cause});
+}
+
+class RuntimeError extends WasmError {
+  RuntimeError(super.message, {super.cause});
 }
